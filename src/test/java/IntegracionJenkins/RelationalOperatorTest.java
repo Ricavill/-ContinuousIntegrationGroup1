4f7873ba9b52b05532feacf3d97284c3dab3c145
josalcab <josalcab@espol.edu.ec>
2021-07-25 20:49:29 -0500

    Corrección en los tests existentes y nuevos tests agregados.

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -17,6 +17,12 @@ public class RelationalOperatorTest {
 		RelationalOperator tester = new RelationalOperator();
 		assertTrue(tester.isGreater(2, 1));
 	}
+	
+	@Test
+	public void isGreaterTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertTrue(tester.isGreater(2, 2));
+	}
 
 	@Test
 	public void isLessTest1() {
@@ -29,4 +35,11 @@ public class RelationalOperatorTest {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
 	}
+	
+	@Test
+	public void isLessTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isLess(5, 7));
+	}
+	
 }
